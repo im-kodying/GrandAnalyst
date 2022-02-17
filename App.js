b9696d93800e85fca2a69f813b36668eca668b3c
KodyCoppock <kodycoppock@gmail.com>
2022-02-17 16:18:41 -0500

    non-responsive chessboard

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SquareStyles from "./components/SquareStyles";
+import {useState, useEffect} from "react";
+import { useWindowDimensions } from 'react-native';
 
 export default function App() {
+    const [chessboardSize, setChessboardSize] = useState(undefined);
+    const [selectedBoard, setSelectedBoard] = useState('PlayVsRandom');
+    const { height, width } = useWindowDimensions();
   return (
     <View style={styles.container}>
       <h2>Move Options</h2>
-      <SquareStyles boardWidth={1080} />
+      <SquareStyles boardWidth={width/2} />
         <br />
       <StatusBar style="auto" />
     </View>
