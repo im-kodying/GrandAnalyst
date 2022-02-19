cab51345b147eae594066d34c11b7947d96c2bcc
KodyCoppock <kodycoppock@gmail.com>
2022-02-18 23:48:51 -0500

    Numbered PGN list

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import {StyleSheet, Text, TextComponent, View} from 'react-native';
 import SquareStyles from "./components/SquareStyles";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import {useState, useEffect} from "react";
 import { useWindowDimensions } from 'react-native';
 import Container from 'react-bootstrap/Container';
@@ -9,15 +9,23 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from "react-bootstrap/Navbar";
 import Logo from "./assets/GA.svg"
+import ListGroup from "react-bootstrap/ListGroup";
+import {ListGroupItem} from "react-bootstrap";
 
 export default function App() {
     const [pgn, setPgn] = useState([]);
+    let arr = pgn.reduce(function(result, value, index, array) {
+        if (index % 2 === 0)
+            result.push(array.slice(index, index + 2));
+        return result;
+    }, []);
+    const list = arr.map((move, index) => <Row><Col>{index+1}.</Col><Col>{move[0]}</Col><Col>{move[1]}</Col></Row>);
     function addPgn(move){
         setPgn([...pgn, move]) //simple value
     }
   return (
       <>
-      <Navbar bg="dark" variant="dark" sticky={"top"}>
+          <Navbar bg="dark" variant="dark" sticky={"top"}>
           <Container>
               <Navbar.Brand href="#home">
                   <img
@@ -27,7 +35,7 @@ export default function App() {
                       height="30"
                       className="d-inline-block align-top"
                   />{' '}
-                  Grand Analyst
+                  GrandAnalyst
               </Navbar.Brand>
           </Container>
       </Navbar>
@@ -35,7 +43,7 @@ export default function App() {
           <Row>
               <Col></Col>
               <Col><SquareStyles pgn={pgn} addPgn={addPgn}/></Col>
-              <Col>{pgn}</Col>
+              <Col>{list}</Col>
           </Row>
       </Container>
           </>
